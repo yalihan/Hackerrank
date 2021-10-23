8560d5e0afb3f2c57dc927bbb9c681b45e070065
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-23 11:42:51 +0300

    Added, Day1_DataTypes

---
+++ Practice/30DaysOfCode/Day1_DataTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        int i2 = Convert.ToInt32(Console.ReadLine());
+        double d2 = Convert.ToDouble(Console.ReadLine());
+        string s2 = Console.ReadLine();
+
+        string d3 = String.Format("{0:0.0}", d + d2);
+        Console.WriteLine(i + i2);
+        Console.WriteLine(d3);
+        Console.WriteLine(s + s2);
+        // Declare second integer, double, and String variables.
+
+        // Read and save an integer, double, and String to your variables.
+
+        // Print the sum of both integer variables on a new line.
+
+        // Print the sum of the double variables on a new line.
+
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+
+    }
+}
