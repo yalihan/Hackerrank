9a7a399ac3ffe8ae8b199bc628cbd4883d37976a
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-28 09:17:34 +0300

    Added, CorrectnessAndTheLoopInvariant

---
+++ Practice/ProblemSolving/CorrectnessAndTheLoopInvariant/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Solution
+{
+    public static void insertionSort(int[] A)
+    {
+        Array.Sort(A);
+        foreach (int element in A)
+            Console.Write(element + " ");
+    }
+
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        int[] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+        insertionSort(_ar);
+    }
+}
+
