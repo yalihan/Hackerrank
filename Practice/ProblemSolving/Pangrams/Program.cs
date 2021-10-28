f65aa6bad077ef018589f5c08b53d158b9c0cb36
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-28 09:44:55 +0300

    Added, Pangrams

---
+++ Practice/ProblemSolving/Pangrams/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+        s = s.ToLower();
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        foreach(char letter in alphabet)
+        {
+            if (!s.Contains(letter))
+                return "not pangram";
+        }
+        return "pangram";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
