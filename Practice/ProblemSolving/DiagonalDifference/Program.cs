73b0078fc99bce6b4fbed9daacc8aa2ac068f353
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-28 09:07:25 +0300

    Added, DiagonalDifference

---
+++ Practice/ProblemSolving/DiagonalDifference/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int rToL = 0;
+        int lToR = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            lToR += arr[i][i];
+            rToL += arr[arr.Count-1-i][i];
+            for (int j = 0; j < arr[i].Count; j++)
+            {
+            }
+        }
+        int difference = lToR - rToL;
+        if (difference > 0)
+            return difference;
+        return -difference;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
